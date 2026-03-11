9d910b0bebd66aadd271bf164becd74468b9a0f8
Dimavo42 <dimaiscool95@gmail.com>
2026-03-11 21:23:28 +0200

    - added a simple cores for react app

--- backend/TradeWebAPI/Program.cs
+++ backend/TradeWebAPI/Program.cs
@@ -8,8 +8,6 @@ using TradeWebAPI.Services.Implementations;
 using TradeWebAPI.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
-
-
 builder.Services.AddAutoMapper(typeof(TradeProfile));
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Sql")));
@@ -24,6 +22,17 @@ builder.Services.AddScoped<IStockService, StockService>();
 builder.Services.AddScoped<ITradeValidator, TradeValidator>();
 builder.Services.AddScoped<ITradeRepository, TradeRepository>();
 builder.Services.AddScoped<IStockRepository, StockRepository>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("frontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 var app = builder.Build();
 
 app.UseMiddleware<RequestLoggingMiddleware>();
@@ -42,8 +51,13 @@ else
 
  app.UseHttpsRedirection();
 
+app.UseCors("frontend");
+
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+
+
